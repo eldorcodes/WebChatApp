259d44eda2ff5feb0cc1c4c7584bdb3bafa20345
Eldor Togaymurotov <eldorcodes@gmail.com>
2018-12-14 16:50:23 -0600

    confir vars added

--- config/prod.js
+++ config/prod.js
@@ -2,8 +2,8 @@ module.exports = {
     MongoURI: process.env.MONGO_URI,
     FacebookAppID: process.env.FACEBOOK_APP_ID,
     FacebookAppSecret: process.env.FACEBOOK_APP_SECRET,
-    GoogleClientID: '260023116052-i77jub1709d3p3g9lg601nupesvev1l9.apps.googleusercontent.com',
-    GoogleClientSecret: 'qlyPVWclhNKRuudo0EAhQC96',
-    StripePublishableKey: 'pk_test_LJlgLqTqXjvYa3tyT4JKVqzd',
-    StripeSecretKey: 'sk_test_BlySWcEvYTb90AbIGQMijp6n'
+    GoogleClientID: process.env.GOOGLE_CLIENT_ID,
+    GoogleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    StripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+    StripeSecretKey: process.env.STRIPE_SECRET_KEY
 };
