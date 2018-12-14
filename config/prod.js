bc7629399bb49191be68e33834453cf891e24063
Eldor Togaymurotov <eldorcodes@gmail.com>
2018-12-13 20:55:05 -0600

    facebook and mongouri prod

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,7 @@
 module.exports = {
-    MongoURI: 'mongodb://messenger:messenger1@ds135233.mlab.com:35233/messenger-dev',
-    FacebookAppID: '494856334360426',
-    FacebookAppSecret: '356a63407814b59a937f32d00751316e',
+    MongoURI: process.env.MONGO_URI,
+    FacebookAppID: process.env.FACEBOOK_APP_ID,
+    FacebookAppSecret: process.env.FACEBOOK_APP_SECRET,
     GoogleClientID: '260023116052-i77jub1709d3p3g9lg601nupesvev1l9.apps.googleusercontent.com',
     GoogleClientSecret: 'qlyPVWclhNKRuudo0EAhQC96',
     StripePublishableKey: 'pk_test_LJlgLqTqXjvYa3tyT4JKVqzd',
